4f2ce13823951d98a178fa88216f7c7a1af94368
ndrosis <drosis.nikos@gmail.com>
2020-10-24 00:30:50 +0300

    update mydata module class

--- core/modules/modmydata_module.class.php
+++ core/modules/modmydata_module.class.php
@@ -31,4 +31,102 @@
  * Description and activation class for module numberseries
  */
 class modmydata_module extends DolibarrModules
- 
+ {
+
+	/**
+	 *   Constructor. Define names, constants, directories, boxes, permissions
+	 *
+	 *   @param		DoliDB		$db		Database handler
+	 */
+	function modmydata_module($db)
+	{
+        global $langs,$conf;
+
+        $this->db = $db;
+		$this->numero = 130005;
+
+		$this->family = "other";
+		// Module label (no space allowed), used if translation string 'ModuleXXXName' not found (where XXX is value of numeric property 'numero' of module)
+		$this->name = "My Data";
+		$this->description = "Add mydata into database (Greece)";
+
+		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
+		$this->version = '1.0.0';
+		$this->const_name = 'MAIN_MODULE_'.strtoupper($this->name);
+		$this->special = 2;
+		$this->picto='bill';
+
+		// Data directories to create when module is enabled
+		$this->dirs = array();
+
+		// Dependances
+		$this->depends = array();
+		$this->langfiles = array('mydata_module@mydata_module');
+
+		// Config pages
+        $this->config_page_url = array("setup.php@mydata_module");
+
+        // Constants
+        // List of particular constants to add when module is enabled (key, 'chaine', value, desc, visible, 'current' or 'allentities', deleteonunactive)
+        // Example: $this->const=array(0=>array('MYMODULE_MYNEWCONST1','chaine','myvalue','This is a constant to add',1),
+        //                             1=>array('MYMODULE_MYNEWCONST2','chaine','myvalue','This is another constant to add',0) );
+        //                             2=>array('MAIN_MODULE_MYMODULE_NEEDSMARTY','chaine',1,'Constant to say module need smarty',1)
+		$this->const = array(0=>array('MAIN_USE_MYDATA_DICTIONNARY','chaine','1','Constant to enable usage of mydata tables',0,'current',1));
+
+        // Boxes
+		$this->boxes = array();
+
+		// Permissions
+		$this->rights = array();
+
+	}
+
+
+	/**
+	 *		Function called when module is enabled.
+	 *		The init function add constants, boxes, permissions and menus (defined in constructor) into Dolibarr database.
+	 *		It also creates data directories
+	 *
+     *      @param      string	$options    Options when enabling module ('', 'noboxes')
+	 *      @return     int             	1 if OK, 0 if KO
+	 */
+	function init($options='')
+	{
+		global $conf;
+		// Permissions et valeurs par defaut
+		$this->remove($options);
+
+		$sql = array();
+
+		$result=$this->load_tables();
+
+		return $this->_init($sql,$options);
+	}
+
+	/**
+	 *		Function called when module is disabled.
+	 *      Remove from database constants, boxes and permissions from Dolibarr database.
+	 *		Data directories are not deleted
+	 *
+     *      @param      string	$options    Options when enabling module ('', 'noboxes')
+	 *      @return     int             	1 if OK, 0 if KO
+	 */
+	function remove($options='')
+	{
+		$sql = array();
+
+		return $this->_remove($sql,$options);
+	}
+
+	/**
+	 *	Create tables and keys required by module
+	 *	This function is called by this->init.
+	 *
+	 *	@return		int		<=0 if KO, >0 if OK
+	 */
+	function load_tables()
+	{
+		return $this->_load_tables('/mydata_module/sql/');
+	}
+}
+?>
