2be32af3affe80b7bc118d8795a91fa7db57ff28
Dimitris Kalivis <51862140+dkalivis@users.noreply.github.com>
2020-10-25 13:11:12 +0200

    Create llx_c_mydata_feespercentcategory.sql

---
+++ sql/llx_c_mydata_feespercentcategory.sql
@@ -0,0 +1,31 @@
+-- ========================================================================
+-- Copyright (C) 2020 George Kourtis 
+-- Copyright (C) 2020 Nikos Drosis <ndrosis@sysaid.gr>
+-- Copyright (C) 2020 Dimitris Kalivis <dkalivis@technoex.gr>
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program. If not, see <https://www.gnu.org/licenses/>.
+--
+-- ========================================================================
+
+CREATE TABLE `llx_c_mydata_feespercentcategory` (
+  `rowid` integer NOT NULL AUTO_INCREMENT,
+  `code` varchar(5) COLLATE utf8_unicode_ci NOT NULL,
+  `description` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `fee` decimal(4,2) NOT NULL,
+  `fee_type` tinyint NOT NULL,
+  `active` tinyint NOT NULL DEFAULT '1',
+  PRIMARY KEY (`rowid`)
+) ENGINE=InnoDB;
+
+-- fee_type takes 1 for percent or 2 for ammount
